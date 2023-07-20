65e8b37c126fd5fb0285ebd295ce2fa6fc7bab80
burcukocal~ <burcukocal97@gmail.comgit config --global user.name burcukocal~git config --global user.email burcukocal97@gmail.com>
2023-07-20 12:39:01 +0300

    .

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors:{
+        taskHeader:"#c8781a",
+        description: "#444",
+        button: "#fecc91"
+
+      }
+    },
+  },
+  plugins: [],
+}
+
